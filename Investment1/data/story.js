ea500b53c062b871db39365c2ee3b4dbbdeb9b43
Allan McEvoy <allan.a.mcevoy@gmail.com>
2023-10-05 15:00:50 +0100

    Add files via upload

---
+++ Investment1/data/story.js
@@ -0,0 +1,153 @@
+var story = {
+ "docName": "💸 Investment",
+ "docPath": "P_P_P",
+ "docVersion": 100000001,
+ "ownerName": "",
+ "ownerEmail": "",
+ "authorName": "V_V_N",
+ "authorEmail": "V_V_E",
+ "fileType": "png",
+ "disableInteractions": false,
+ "highlightHotspot": true,
+ "highlightAllHotspots": true,
+ "hideGallery": false,
+ "zoomEnabled": true,
+ "cloud": false,
+ "title": "💸 Investment",
+ "layersExist": true,
+ "pages": [
+  {
+   "id": "1:21",
+   "groupID": "1:16",
+   "transAnimType": 0,
+   "title": "Thumbnail",
+   "image": "thumbnail.png",
+   "index": 0,
+   "width": 1920,
+   "height": 1080,
+   "x": -960,
+   "y": -540,
+   "isFrame": true,
+   "type": "regular",
+   "fixedPanels": [],
+   "links": [],
+   "overlayPinPage": 6,
+   "overlayOutsideClickIgnore": true,
+   "layout": null
+  },
+  {
+   "id": "1:2",
+   "groupID": "0:1",
+   "transAnimType": 0,
+   "title": "Slide 16:9 - 1",
+   "image": "slide-16-9-1.png",
+   "index": 1,
+   "width": 1920,
+   "height": 1080,
+   "x": -959,
+   "y": -539,
+   "isFrame": true,
+   "type": "regular",
+   "fixedPanels": [],
+   "links": [
+    {
+     "name": "Slide 16:9 - 1",
+     "rect": {
+      "x": 0,
+      "y": 0,
+      "width": 1920,
+      "height": 1080
+     },
+     "index": 0,
+     "page": 2
+    }
+   ],
+   "overlayPinPage": 6,
+   "overlayOutsideClickIgnore": true,
+   "layout": null
+  },
+  {
+   "id": "1:4",
+   "groupID": "0:1",
+   "transAnimType": 0,
+   "title": "Slide 16:9 - 2",
+   "image": "slide-16-9-2.png",
+   "index": 2,
+   "width": 1920,
+   "height": 1080,
+   "x": 1350,
+   "y": -540,
+   "isFrame": true,
+   "type": "regular",
+   "fixedPanels": [],
+   "links": [
+    {
+     "name": "Slide 16:9 - 2",
+     "rect": {
+      "x": 0,
+      "y": 0,
+      "width": 1920,
+      "height": 1080
+     },
+     "index": 1,
+     "page": 3
+    }
+   ],
+   "overlayPinPage": 6,
+   "overlayOutsideClickIgnore": true,
+   "layout": null
+  },
+  {
+   "id": "1:8",
+   "groupID": "0:1",
+   "transAnimType": 0,
+   "title": "Slide 16:9 - 3",
+   "image": "slide-16-9-3.png",
+   "index": 3,
+   "width": 1920,
+   "height": 1080,
+   "x": 3660,
+   "y": -540,
+   "isFrame": true,
+   "type": "regular",
+   "fixedPanels": [],
+   "links": [],
+   "overlayPinPage": 6,
+   "overlayOutsideClickIgnore": true,
+   "layout": null
+  }
+ ],
+ "groups": [
+  {
+   "id": "1:16",
+   "name": "Cover",
+   "backColor": "#F5F5F5"
+  },
+  {
+   "id": "1:17",
+   "name": "------------",
+   "backColor": "#F5F5F5"
+  },
+  {
+   "id": "1:18",
+   "name": "Research",
+   "backColor": "#F5F5F5"
+  },
+  {
+   "id": "0:1",
+   "name": "Portfolio Flatplan",
+   "backColor": "#F5F5F5"
+  },
+  {
+   "id": "1:20",
+   "name": "------------",
+   "backColor": "#F5F5F5"
+  },
+  {
+   "id": "1:19",
+   "name": "Notes",
+   "backColor": "#F5F5F5"
+  }
+ ],
+ "totalImages": 4
+}
